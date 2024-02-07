c18c7e969fa07d1a728eadd24191244d2e3f66de
Bugado <addminplayerbr1122@gmail.com>
2024-02-07 16:31:12 -0300

    feat: Small additions

--- src/br/univali/bank/core/Bank.java
+++ src/br/univali/bank/core/Bank.java
@@ -7,10 +7,14 @@ import java.util.List;
 public class Bank {
 
     private List<Account> accounts;
-
     private List<Customer> customers;
-
     private List<Transaction> transactions;
+
+    public Bank(List<Account> accounts, List<Customer> customers, List<Transaction> transactions) {
+        this.accounts = accounts;
+        this.customers = customers;
+        this.transactions = transactions;
+    }
 
     public List<Transaction> getTransactions(){
         return transactions;
--- src/br/univali/bank/core/Transaction.java
+++ src/br/univali/bank/core/Transaction.java
@@ -3,6 +3,7 @@ import br.univali.bank.enums.TransactionType;
 
 import java.util.Date;
 public class Transaction{
+
     private String transactionID;
     private double amount;
     private Date transactionDate;
@@ -10,21 +11,35 @@ public class Transaction{
     private Account sourceAccount;
     private Account destinationAccount;
 
+    public Transaction(String transactionID, double amount, Date transactionDate, TransactionType transactionType, Account sourceAccount, Account destinationAccount) {
+        this.transactionID = transactionID;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.transactionType = transactionType;
+        this.sourceAccount = sourceAccount;
+        this.destinationAccount = destinationAccount;
+    }
+
     public String getTransactionID(){
         return transactionID;
     }
+
     public double getAmount(){
         return amount;
     }
+
     public Date getTransactionDate(){
         return transactionDate;
     }
+
     public TransactionType getTransactionType(){
         return transactionType;
     }
+
     public Account getSourceAccount(){
         return sourceAccount;
     }
+
     public Account getDestinationAccount(){
         return destinationAccount;
     }
